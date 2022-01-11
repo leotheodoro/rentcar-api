008bc14663c967b0d98dc3d5249a0109739f6cec
Leonardo Theodoro <leonardo@theodoro.dev>
2022-01-11 20:35:50 -0300

    Creating categories repository and listing all categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,20 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.status(200).json(all);
+});
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
+  const category = categoriesRepository.create({ name, description });
 
   return response.status(201).json(category);
 });
