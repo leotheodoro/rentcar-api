7f267865664ce9704e894de8b41451d8d716c05b
Leonardo Theodoro <leonardo@recruitrobin.com>
2022-01-17 15:41:17 -0300

    swagger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -8,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
-
 
 app.listen(3333, () =>
   console.log("Server is running on http://localhost:3333 🚀")
