36bbd24189607cd947c650431b092e3080a99133
Leonardo Theodoro <leonardo@recruitrobin.com>
2022-01-25 09:23:15 -0300

    ensuring user is authenticated middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error("Token missing");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "bbd918962829d3672017999c1a1080dc"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists");
+    }
+
+    next();
+  } catch (error) {
+    throw new Error("Invalid Token");
+  }
+}
